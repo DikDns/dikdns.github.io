cf5d70bdbcc748806fa233caee85aa6c557654e9
Andika Eka Kurnia <dikdnssocial@gmail.com>
2021-06-23 10:46:44 +0700

    Add hamburger menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Hamburger Menu Script
+const menuToggle = document.querySelector('.nav-menu-toggle input');
+const nav = document.querySelector('.nav .nav-link');
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide')
+});
