d6eaf7647a21f53c95774ff9cde6a7cb38738109
Andika Eka Kurnia <dikdnssocial@gmail.com>
2021-06-30 10:48:06 +0700

    copy script from the sub page andika-eka-kurnia

--- js/script.js
+++ js/script.js
@@ -8,8 +8,28 @@ menuToggle.addEventListener('click', function(){
 
 
 
+// select button
+var btn1 = document.getElementById("dropdown-1");
+var arrow1 = document.getElementById('arrow-1');
+
+
+// Button function
+function dropdownBtnLang() {
+    btn1.classList.toggle("show-the-link");
+};
+
+
 // Close it if the user click outside of it
 window.onclick = function(event) {
+    if (!event.target.matches('.lang-button')){
+        let dropdowns = document.getElementsByClassName("list");
+        for(let i = 0; i < dropdowns.length; i++){
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show-the-link')) {
+                openDropdown.classList.remove('show-the-link');
+            }
+        }
+    }
     if (!event.target.matches('.btn-toggle')){
         nav.classList.remove('slide');
         document.querySelector('.btn-toggle').checked = false;
