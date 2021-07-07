83912ab93fe617e848426139a7f5d1030e4ffe4e
Andika Eka Kurnia <dikdnssocial@gmail.com>
2021-07-07 11:09:22 +0700

    add onscroll event navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    document.getElementById('nav').style.top = "0";
+  } else {
+    document.getElementById('nav').style.top = "-100px";
+  }
+}
+
+
+
+
+
 // Hamburger Menu Script
 const menuToggle = document.querySelector('.nav-menu-toggle input');
 const nav = document.querySelector('.nav .nav-link');
