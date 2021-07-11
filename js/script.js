11006166b44c7ddf6b45506a8d9e2eb9745e23e6
Andika Eka Kurnia <dikdnssocial@gmail.com>
2021-07-11 11:01:48 +0700

    fix bug nav on mobile when open the menu link

--- js/script.js
+++ js/script.js
@@ -1,41 +1,46 @@
-// When the user scrolls down 250px from the top of the document, slide down the navbar
-// When the user scrolls to the top of the page, slide up the navbar (250px out of the top view)
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-    document.getElementById('nav').style.top = "0";
-  } else {
-    document.getElementById('nav').style.top = "-100px";
-  }
-}
-
-
-
-
-
-// Hamburger Menu Script
+//! Select Elements
+// Nav Hamburger and scroll
+const nav = document.getElementById('nav');
 const menuToggle = document.querySelector('.nav-menu-toggle input');
-const nav = document.querySelector('.nav .nav-link');
-
-menuToggle.addEventListener('click', function(){
-    nav.classList.toggle('slide')
-});
-
-
-//! Language Script 
-// select language
+const navLink = document.querySelector('.nav .nav-link');
+// language
 const langBtn = document.querySelector(".lang-button");
 const langList = document.querySelector(".lang-dropdown .list");
 const langArrow = document.querySelector('.lang-button .arrow-icon');
 
-// Button function to run
+
+
+//! NAV script
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if  (!document.querySelector('.nav-link').classList.contains('slide')){
+        if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+            nav.classList.add('active');
+        } else {
+            nav.classList.remove('active');
+        }
+    }
+}
+
+
+
+//! Hamburger Menu Script
+menuToggle.addEventListener('click', function(){
+    navLink.classList.toggle('slide')
+});
+
+
+
+//! Language Script 
 langBtn.addEventListener('click', function(){
     langList.classList.toggle("show-the-link");
     langArrow.classList.toggle("arrow-rotate");
 });
 
-// Close it if the user click outside of it
+
+
+//! Close it if the user click outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.lang-button')){
         let dropdowns = document.getElementsByClassName("list");
